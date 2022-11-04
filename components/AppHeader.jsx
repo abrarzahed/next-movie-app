8fb163b036457237baacc8ccd8ec04a05bd78093
abrarzahed <abrarzahed6986@gmail.com>
2022-11-04 16:17:15 +0600

    added heroIcons

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import {
+  HomeIcon,
+  UserIcon,
+  PhoneIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/solid";
+
 export default function AppHeader() {
-  return <div>AppHeader</div>;
+  return (
+    <div>
+      <div>
+        <HeaderIcon Icon={HomeIcon} title="Home" />
+        <HeaderIcon Icon={UserIcon} title="Account" />
+        <HeaderIcon Icon={InformationCircleIcon} title="About" />
+        <HeaderIcon Icon={PhoneIcon} title="Contact" />
+      </div>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+        width={100}
+        height={100}
+        alt="IMDb LOGo"
+      />
+    </div>
+  );
 }
