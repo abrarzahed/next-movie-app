acef486822568476a53bbe6de522a6cba3f1f34a
abrarzahed <abrarzahed6986@gmail.com>
2022-11-04 16:55:23 +0600

    styled header component

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -9,14 +9,15 @@ import {
 
 export default function AppHeader() {
   return (
-    <div>
-      <div>
+    <div className="bg-slate-700 p-6 select-none text-gray-300 flex flex-col justify-center sm:flex-row sm:justify-between md:items-center items-center">
+      <div className="flex flex-wrap space-x-4 justify-center ">
         <HeaderIcon Icon={HomeIcon} title="Home" />
         <HeaderIcon Icon={UserIcon} title="Account" />
         <HeaderIcon Icon={InformationCircleIcon} title="About" />
         <HeaderIcon Icon={PhoneIcon} title="Contact" />
       </div>
       <Image
+        className="mt-6 sm:mt-0  md:mt-0"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={100}
         height={100}
--- components/HeaderIcon.jsx
+++ components/HeaderIcon.jsx
@@ -1,8 +1,8 @@
 export default function HeaderIcon({ Icon, title }) {
   return (
-    <div>
-      <Icon className="h-8" />
-      <p>{title}</p>
+    <div className="flex space-y-2 flex-col justify-center">
+      <Icon className="h-7" />
+      <p className="font-medium text-xs uppercase">{title}</p>
     </div>
   );
 }
