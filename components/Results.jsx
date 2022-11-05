8f8b9379434e00052b073b1db8e310e4bdeca759
abrarzahed <abrarzahed6986@gmail.com>
2022-11-05 10:37:23 +0600

    created server side function

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,3 +1,9 @@
-export default function Results() {
-  return <div>Results</div>;
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((r, i) => (
+        <h1 key={i}>{r.title}</h1>
+      ))}
+    </div>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,10 @@ import Head from "next/head";
 import AppHeader from "../components/AppHeader";
 import Navbar from "../components/Navbar";
 import Results from "../components/Results";
+import requests from "../utilities/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -19,7 +21,22 @@ export default function Home() {
       <Navbar />
 
       {/* Results: Movies */}
-      <Results />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre || "fetchTrending";
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre].url}`
+  ).then((res) => {
+    return res.json();
+  });
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
